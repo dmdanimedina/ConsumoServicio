a5c5cf4dd66602a0c464eed79dad1a4c66712a6d
Daniel Medina <dmdanimedina@gmail.com>
2016-07-09 13:45:42 -0400

    aplicacion terminada

--- app/src/main/java/com/example/danimedina/consumoservicio/MainActivity.java
+++ app/src/main/java/com/example/danimedina/consumoservicio/MainActivity.java
@@ -2,12 +2,43 @@ package com.example.danimedina.consumoservicio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+
+import org.json.JSONException;
+import cz.msebera.android.httpclient.Header;
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    public static String ruta = "http://betacode.cl/BetacodeLabsMasterRest/Master/public/Lovers";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+
+        AsyncHttpClient client = new AsyncHttpClient();
+
+        client.get(ruta,null, new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                try {
+                    JSONArray pers = response.getJSONArray("data");
+                    for(int i = 0; i<pers.length(); i++){
+                        JSONObject c = pers.getJSONObject(i);
+                        Log.d("_____DATA______",c.getString("nombrecompleto"));
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }});
+
     }
+
 }
